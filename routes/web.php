bcff6818f6611ba7294bc1d958cc9f0ec884dfb1
m-salaun-esimed <msalaun@esimed.fr>
2025-06-23 15:43:33 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-Route::get('/admin/login', function () {
-    return 'Admin Login Page';
-})->name('filament.admin.auth.login');
-    
