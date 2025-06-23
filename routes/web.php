a7092368af9adbfb0c8605ac5c9ac25bfa9f513b
m-salaun-esimed <msalaun@esimed.fr>
2025-06-23 13:38:50 +0200

    start dev tests

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'show'])->name('projects.show');
---
+++ tests/Feature/CreateUserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_be_created_directly()
+    {
+        $user = \App\Models\User::factory()->create([
+            'email' => 'testuser@example.com',
+            'password' => bcrypt('password'),
+        ]); 
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'testuser@example.com',
+        ]);
+    }
+
+}
