a561c3fdc291be3c03300b1f41d5329717be79d1
m-salaun-esimed <msalaun@esimed.fr>
2025-06-25 15:46:12 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (isset($_ENV['PLATFORM_APPLICATION_NAME']) && !isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
+            return;
+        }
     }
 
     /**
